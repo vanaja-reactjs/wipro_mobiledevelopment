4b629b2cb514f79ccbf30bd37f0a501d86541e18
vanaja-reactjs <kandrugulavanaja@gmail.com>
2024-06-21 16:45:56 +0530

    shell programming assignments h

--- assignment1.sh
+++ assignment1.sh
@@ -1,20 +1,8 @@
-#!bin/bash
-while true;  do
-    read -p "enter a number(enter 0 to stop): " num
-    if ! [[ "$num" =~ ^[0-9]+$ ]]; then
-        echo "Invalid input please enetr a valid number"
-        continue
-    fi
-
-num=$((num))
-    if [ "$num" -eq 0 ]; then 
-         echo "exiting the program"
-         break
-    fi
-    if [ $((num % 2)) -eq 0 ]; then
-        echo "$num is even"
-    else
-        echo "$num is odd"
-    fi
-done
-
+#!/bin/bash
+echo eneter file name 
+read file
+if [ -e "$file" ]; then
+    echo file exists
+else
+    echo file not exist
+fi
--- assignment2.sh
+++ assignment2.sh
@@ -1,8 +1,20 @@
-#!/bin/bash
-echo eneter file name 
-read file
-if [ -e "$file" ]; then
-    echo file exists
-else
-    echo file not exist
-fi
+#!bin/bash
+while true;  do
+    read -p "enter a number(enter 0 to stop): " num
+    if ! [[ "$num" =~ ^[0-9]+$ ]]; then
+        echo "Invalid input please enetr a valid number"
+        continue
+    fi
+
+num=$((num))
+    if [ "$num" -eq 0 ]; then 
+         echo "exiting the program"
+         break
+    fi
+    if [ $((num % 2)) -eq 0 ]; then
+        echo "$num is even"
+    else
+        echo "$num is odd"
+    fi
+done
+
